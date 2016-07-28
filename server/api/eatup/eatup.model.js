345d3ca90c7d5cab2e341ccf70323867e1c27aff
Tee Blagdon <tjlay23@gmail.com>
2016-07-28 14:12:26 -0700

    Feat: Create API route to GET a specific eatUp /api/eatup/:id and post new restaurant

--- server/api/eatup/eatup.model.js
+++ server/api/eatup/eatup.model.js
@@ -2,9 +2,8 @@ const Eatup = function(db, DataTypes) {
 
   return db.define('Eatup', {
     title: DataTypes.STRING,
-    startTime: DataTypes.TIME,
-    endTime: DataTypes.TIME,
-    date: DataTypes.DATE,
+    startTime: DataTypes.DATE,
+    endTime: DataTypes.DATE,
     description: DataTypes.STRING
   });
 

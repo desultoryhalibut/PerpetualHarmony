711657dfef1b3e2c3883ae27c5d5f7cfa6f511ad
Quin Kinser <quinkinser@gmail.com>
2016-07-29 19:38:19 -0700

    Set up form to get ready to submit ajax request

--- client/app/components/CommentForm.jsx
+++ client/app/components/CommentForm.jsx
@@ -3,13 +3,34 @@ import React from 'react';
 export default class CommentForm extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      comment: ''
+    }
+  }
+
+  handleChange(e) {
+    this.setState({ comment: e.target.value });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    var comment = this.state.comment.trim();
+    if (!comment) {
+      return;
+    }
+    // TODO: send request to the server
+    this.setState({comment: ''});
   }
 
   render() {
     return (
-      <div className="commentForm">
-        I am the CommentForm
-      </div>
+      <form className="commentForm" onSubmit={this.handleSubmit}>
+        <input type="text" placeholder="Leave a comment..."
+          value={this.state.comment}
+          onChange={this.handleComment}
+        />
+        <input type="submit" value="Post" />
+      </form>
     );
   }
 }

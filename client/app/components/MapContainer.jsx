e537b887fe0eaf595ad8d083ddebab7710146d87
Carlos Espinal <cespinalt@gmail.com>
2016-07-30 10:49:37 -0700

    Implement map container

--- client/app/components/MapContainer.jsx
+++ client/app/components/MapContainer.jsx
@@ -1,23 +1,30 @@
-/* import GoogleApiWrapper from './GoogleApi.jsx'; */
-
-
-
 import React, {PropTypes, Component} from 'react';
 import GoogleMap from 'google-map-react';
-// import MyGreatPlace from './my_great_place.jsx';
+import EatUpMarker from './Marker.jsx';
+import {GoogleApiKey} from '../../../server/GoogleApiKey.jsx';
 
-export default class SimpleMapPage extends Component {
-
+export default class Map extends Component {
   constructor(props) {
     super(props);
   }
 
   render() {
+    var eatUps = this.props.eatUps || [];
+    var markers = [];
+      eatUps.forEach(eatUp => {
+        if(eatUp.Restaurant.longitude && eatUp.Restaurant.latitude) {
+          markers.push({lat: eatUp.Restaurant.latitude, lng: eatUp.Restaurant.longitude});
+        }
+      });
     return (
        <GoogleMap
-        bootstrapURLKeys={{ key: 'AIzaSyByBDfpk8mN0VEt2-sIIzteruwERoUkvQo' }}
-        center={[37, -122]}
-        zoom={15}></GoogleMap>
+        bootstrapURLKeys={{ key: GoogleApiKey }}
+        center={[37.798307, -122.408013]}
+        zoom={15}>
+        {markers.map(marker => {
+          return <EatUpMarker lat={marker.lat} lng={marker.lng} text={'A'}></EatUpMarker>
+        })}
+       </GoogleMap>
     );
   }
 }
